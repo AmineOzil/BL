7098e5c41561ebf06e21e3c0f191009b9be68630
Amine <aminbouali1995@hotmail.com>
2020-12-08 18:14:27 +0100

    Comments added

--- test.ts
+++ test.ts
@@ -1,34 +1,37 @@
 import {FunctionDeclaration, Project} from 'ts-morph'
-import { isFunctionDeclaration, isIdentifier } from 'typescript';
-async function test() {
+
+async function injectLPS() {
     const project = new Project({
-      tsConfigFilePath:"/home/depinfo/Bureau/JeuxDeMotsTesting/design-dico/tsconfig.json",
+      tsConfigFilePath:"/home/depinfo/Bureau/JeuxDeMotsTesting/design-dico/tsconfig.json", //Passing the absolute path of my tsconfig.js file
   });
-  const modules = project.getAmbientModules();
-    // modules.forEach((modul)=>
-    // console.log(modul.getName));
-    // console.log(modules.length);
-    // console.log(project);
+  //a simple log statement to confirm that the project is correctly loaded
+  // console.log(project); 
+   
+    //Get the source files of my project
     const files=project.getSourceFiles();    
-    // console.log(files);
+    //Log statement to confirm that all the source files were loaded
     console.log(files.length)
+    //Iterating over each file to get the methods contained on his classes
     files.forEach((file)=>{
+            //Log statement to print the names of all source files
             console.log(file.getBaseName())
+            //Getting the classes of source file
            var statements=file.getClasses().forEach((classe)=>
                         classe.getMethods().forEach((method)=>
-                            method.addStatements("console.log(\""+method.getName()+"\")")
+                            
+                            // Adding LPS at the beginning of each method
+                            method.insertStatements(0,"console.log(\""+method.getName()+"\")")
+                            
+                            //  Or adding LPS at the end of each method
+                            //  method.addStatements("console.log(\""+method.getName()+"\")")
                         )
            );
-        //    statements.forEach((statemnt)=>
-        //         {   console.log(isIdentifier(statemnt));
-        //             if(isIdentifier(statemnt)){
-        //             console.log(statemnt.getText());
-        //         }
-        //     });
 
     }    
     );
-    await project.save()
+    console.log("Injection completed");
+    await project.save();
+    console.log("Project files updated!");
   }
 
-  test();
+  injectLPS();
